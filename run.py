f4f4979e7dcc40f9c98bd3a35f306e8e87ecae2c
Arthamna <kanismassang@gmail.com>
2025-05-13 22:22:55 +0700

    use render instead

--- run.py
+++ run.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     logging.info("App Online")
     port = int(os.environ.get("PORT", 8080))
     
+    serve(app, host='0.0.0.0', port=port)
     # development Replit
-    if os.environ.get('REPL_SLUG'):
-        app.run(host='0.0.0.0', port=port)
-    else:
-        serve(app, host='0.0.0.0', port=port)
+    # if not os.environ.get('REPL_SLUG'):
+    #     app.run(host='0.0.0.0', port=port)
+    # else:
     # flask dev
     # app.run(host="0.0.0.0", port=8000)
     
