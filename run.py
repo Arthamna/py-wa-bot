229d5c23c60b5446517dc16656f9460eb63ef94a
Arthamna <kanismassang@gmail.com>
2025-05-13 21:16:21 +0700

    feat : setting replit env

--- run.py
+++ run.py
@@ -1,12 +1,21 @@
 import logging
-
+import os
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == "__main__":
-    logging.info("Flask app started")
-    app.run(host="0.0.0.0", port=8000)
+    logging.info("App Online")
+    port = int(os.environ.get("PORT", 8080))
+    
+    # development Replit
+    if os.environ.get('REPL_SLUG'):
+        app.run(host='0.0.0.0', port=port)
+    else:
+        serve(app, host='0.0.0.0', port=port)
+    # flask dev
+    # app.run(host="0.0.0.0", port=8000)
     
 
 
