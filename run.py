1945a02e73b42c9f6e28c913e1716b8edb455eb3
Arthamna <kanismassang@gmail.com>
2025-05-13 22:49:53 +0700

    vercel

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
-import logging
-import os
+# import logging
+# import os
 from app import create_app
-from waitress import serve
+# from waitress import serve
 
 app = create_app()
 
-if __name__ == "__main__":
-    logging.info("App Online")
-    port = int(os.environ.get("PORT", 8080))
+# if __name__ == "__main__":
+#     logging.info("App Online")
+#     port = int(os.environ.get("PORT", 8080))
     
-    serve(app, host='0.0.0.0', port=port)
+#     serve(app, host='0.0.0.0', port=port)
     # development Replit
     # if not os.environ.get('REPL_SLUG'):
     #     app.run(host='0.0.0.0', port=port)
